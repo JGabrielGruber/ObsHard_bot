a78130d48dac75deecc47cb9b01e3afc7e32476a
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2020-03-01 20:22:12 -0300

    Implementado conexão com o firebase;

--- obsgpu_bot/main.py
+++ obsgpu_bot/main.py
@@ -1,8 +1,12 @@
 import tweepy
 import json
+import firebase_admin
+
+from firebase_admin import db, credentials
 
 __CONFPATH__ = "../config.json"
-api: tweepy.API = None
+tw_api: tweepy.API = None
+fb_app: firebase_admin.App = None
 
 
 def loadConfigs():
@@ -14,7 +18,8 @@ def loadConfigs():
 
 
 def defineConfigs():
-	global api
+	global tw_api
+	global fb_app
 
 	confs = loadConfigs()
 	if confs:
@@ -25,7 +30,12 @@ def defineConfigs():
 			    confs["TWITTER_API"]["ACCESS_TOKEN"]["KEY"],
 			    confs["TWITTER_API"]["ACCESS_TOKEN"]["SECRET"])
 
-			api = tweepy.API(auth)
+			tw_api = tweepy.API(auth)
+
+			fb_cre = firebase_admin.credentials.Certificate(
+			    confs["FIREBASE_API"]["SERVICEACOUNTKEY_PATH"])
+			fb_app = firebase_admin.initialize_app(
+			    fb_cre, {"databaseURL": confs["FIREBASE_API"]["DATABASE_URL"]})
 		except ValueError as e:
 			raise Exception(
 			    "Please, define the config.json file as the config.json.example!"
