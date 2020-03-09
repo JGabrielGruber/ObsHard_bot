ca94220a0a79d82fdb970da0c69152ed77d6e1fb
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2020-03-09 16:41:22 -0300

    Implementado teste de get e add para marca;

---
+++ obsgpu_bot/tests/test_repo_marca.py
@@ -0,0 +1,19 @@
+from models.marca import Marca
+from repositories import marca
+import main
+
+import inspect
+from firebase_admin.db import Reference
+
+def test_get():
+	main.defineConfigs()
+	assert type(marca.get()) is tuple
+
+def test_add():
+	main.defineConfigs()
+	t = marca.add(
+		Marca(
+			"teste"
+		)
+	)
+	assert type(t) is tuple
