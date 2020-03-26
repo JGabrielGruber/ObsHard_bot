f615035cdb4eddb8e8c4338b5f3141387d2863d1
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-03-26 10:45:47 -0300

    Criado teste de update e remoção Marca;

--- obsgpu_bot/tests/test_repo_marca.py
+++ obsgpu_bot/tests/test_repo_marca.py
@@ -5,12 +5,15 @@ import main
 import inspect
 from firebase_admin.db import Reference
 
+id: str = None
+
 def test_get():
 	if main.fb_app is None:
 		main.defineConfigs()
 	assert type(marca.get()) is list
 
 def test_add():
+	global id
 	if main.fb_app is None:
 		main.defineConfigs()
 	t = marca.add(
@@ -18,4 +21,30 @@ def test_add():
 			"teste"
 		)
 	)
+	id = t[1]
 	assert type(t) is tuple
+
+def test_upd():
+	if main.fb_app is None:
+		main.defineConfigs()
+	if id is None:
+		test_add()
+	t = marca.upd(
+		Marca(
+			"testa atualizado",
+			id
+		)
+	)
+	assert type(t) is tuple
+
+def test_rmv():
+	if main.fb_app is None:
+		main.defineConfigs()
+	if id is None:
+		test_add()
+	assert marca.rmv(
+		Marca(
+			"",
+			id
+		)
+	) is None
