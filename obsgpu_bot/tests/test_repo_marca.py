7e9a7d16295322230ba8105e85d0cf6d73203211
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-03-26 09:49:39 -0300

    Corrigido redundância de definição de conf;

--- obsgpu_bot/tests/test_repo_marca.py
+++ obsgpu_bot/tests/test_repo_marca.py
@@ -6,11 +6,13 @@ import inspect
 from firebase_admin.db import Reference
 
 def test_get():
-	main.defineConfigs()
+	if main.fb_app is None:
+		main.defineConfigs()
 	assert type(marca.get()) is list
 
 def test_add():
-	main.defineConfigs()
+	if main.fb_app is None:
+		main.defineConfigs()
 	t = marca.add(
 		Marca(
 			"teste"
