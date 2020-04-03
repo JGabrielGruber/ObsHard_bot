1d49c11da0292f0821d44c0bfc67cb8a3259ef94
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-04-03 10:36:37 -0300

    Refatorado código para instância da classe; Implementado test_getById;

--- obsgpu_bot/tests/test_repo_marca.py
+++ obsgpu_bot/tests/test_repo_marca.py
@@ -7,44 +7,45 @@ from firebase_admin.db import Reference
 
 id: str = None
 
+
 def test_get():
 	if main.fb_app is None:
 		main.defineConfigs()
 	assert type(marca.get()) is list
 
+
 def test_add():
 	global id
 	if main.fb_app is None:
 		main.defineConfigs()
-	t = marca.add(
-		Marca(
-			"teste"
-		)
-	)
-	id = t[1]
-	assert type(t) is tuple
+	t = marca.add(Marca("teste"))
+	id = t._id
+	assert type(t) is Marca
+
+
+def test_getByID():
+	if main.fb_app is None:
+		main.defineConfigs()
+	if id is None:
+		test_add()
+	t = marca.getById(id)
+	test_rmv()
+	assert type(t) is Marca
+
 
 def test_upd():
 	if main.fb_app is None:
 		main.defineConfigs()
 	if id is None:
 		test_add()
-	t = marca.upd(
-		Marca(
-			"testa atualizado",
-			id
-		)
-	)
-	assert type(t) is tuple
+	t = marca.upd(Marca("testa atualizado", id))
+	test_rmv()
+	assert type(t) is Marca
+
 
 def test_rmv():
 	if main.fb_app is None:
 		main.defineConfigs()
 	if id is None:
 		test_add()
-	assert marca.rmv(
-		Marca(
-			"",
-			id
-		)
-	) is None
+	assert marca.rmv(Marca("", id)) is None
