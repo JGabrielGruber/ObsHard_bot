ed2d67dd1c941cc218822af5382459b1e44adebd
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2020-06-11 11:54:17 -0300

    Implementado getProdutos no price request.;

--- obsgpu_bot/price_request/req.py
+++ obsgpu_bot/price_request/req.py
@@ -4,10 +4,12 @@ import locale
 from bs4 import BeautifulSoup
 from models.loja import Loja
 from models.produto import Produto
+from repositories import produto
 
 
 def getPreco(produto: Produto) -> float:
 	locale.setlocale(locale.LC_NUMERIC, "pt_BR.UTF-8")
+	price = {}
 	try:
 		html = requests.get(produto.link)
 		soup = BeautifulSoup(html.text, "html.parser")
@@ -16,8 +18,17 @@ def getPreco(produto: Produto) -> float:
 		    attrs={produto.loja.propriedade: produto.loja.atributo})
 		return float(price["content"])
 	except ValueError:
-		return locale.atof(price["content"])
-	except KeyError:
-		return locale.atof(price.contents[0].string.split(" ", 1)[1])
-	except Exception:
+		try:
+			return locale.atof(price["content"])
+		except KeyError:
+			return locale.atof(price.contents[0].string.split(" ", 1)[1])
+	finally:
 		return 0.0
+
+
+def getProdutos():
+	produtos: list = produto.get()
+	precos: list = []
+	for item in produtos:
+		precos.append(getPreco(item))
+	return precos
