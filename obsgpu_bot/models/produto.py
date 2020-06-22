3da36f436f182616a517e2ba87b27984489e2685
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2020-06-22 20:53:49 -0300

    Adicionados atributos ao Produto;
    Corrigido argumento opcional;

--- obsgpu_bot/models/produto.py
+++ obsgpu_bot/models/produto.py
@@ -7,12 +7,16 @@ class Produto:
 	             loja: Loja,
 	             variacao: Variacao,
 	             link: str,
+	             status: str = "ok",
+	             precos: list = None,
 	             _id: str = None):
 		self._id = _id
 		self.loja = loja
 		self.variacao = variacao
 		self.link = link
+		self.status = status
+		self.precos = precos
 
 	@classmethod
-	def fromJSON(cls, json, id):
-		return cls(None, None, json['link'], id if id else None)
+	def fromJSON(cls, json, id=None):
+		return cls(None, None, json['link'], json.get('status', None), json.get('precos', None), id if id else None)
