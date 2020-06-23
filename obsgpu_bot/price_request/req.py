b57f17676f46e08f7e4e1e225182f791cbd7ad9e
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2020-06-22 21:00:45 -0300

    Aperfeiçoada a lib;
    Implementado tratamentos de erro com status;
    Implementado atualização do preço do produto;
    Implementado atualização do produto se necessário;

--- obsgpu_bot/price_request/req.py
+++ obsgpu_bot/price_request/req.py
@@ -1,16 +1,18 @@
 import requests
 import locale
+import datetime
 
 from bs4 import BeautifulSoup
 from models.loja import Loja
 from models.produto import Produto
-from repositories import produto
+from repositories import produto as rep_produto
 
 
-def getPreco(produto: Produto) -> float:
+def fetchPreco(produto: Produto) -> Produto:
 	locale.setlocale(locale.LC_NUMERIC, "pt_BR.UTF-8")
 	price = {}
 	val = 0.0
+	status = 'ok'
 	try:
 		html = requests.get(produto.link)
 		soup = BeautifulSoup(html.text, "html.parser")
@@ -23,13 +25,26 @@ def getPreco(produto: Produto) -> float:
 			val = locale.atof(price["content"])
 		except KeyError:
 			val = locale.atof(price.contents[0].string.split(" ", 1)[1])
+	except ConnectionError:
+		status = 'off'
+	except Exception as e:
+		print(e)
+		status = 'err'
 	finally:
-		return val
+		produto.status = status
+		if val > 0.0 and (not produto.precos or produto.precos[len(produto.precos) - 1][0] != val):
+			if not produto.precos:
+				produto.precos = []
+			produto.precos.append(
+			    [
+			        val,
+			        datetime.datetime.now().timestamp()
+			    ]
+			)
+		return produto
 
 
 def getProdutos():
-	produtos: list = produto.get()
-	precos: list = []
+	produtos: list = rep_produto.get()
 	for item in produtos:
-		precos.append(getPreco(item))
-	return precos
+		(lambda p : rep_produto.upd(p) if p.precos is item.precos else None)(fetchPreco(item))
--- obsgpu_bot/tests/test_price_request.py
+++ obsgpu_bot/tests/test_price_request.py
@@ -4,8 +4,9 @@ from models.variacao import Variacao
 from models.modelo import Modelo
 from models.arquitetura import Arquitetura
 from models.marca import Marca
-from repositories import modelo
-from repositories import loja
+from repositories import modelo as rep_modelo
+from repositories import loja as rep_loja
+from repositories import produto as rep_produto
 from price_request import req
 import main
 
@@ -15,18 +16,19 @@ from firebase_admin.db import Reference
 id: str = None
 
 
-def test_getPreco():
+def test_fetchPreco():
 	if main.fb_app is None:
 		main.defineConfigs()
 	l = Loja("Kabum", "meta", "itemprop", "price")
-	p = Produto(l, Variacao(Modelo(Marca(""), Arquitetura("", 0), 2020, "teste"), "ASRock B450M-HDV R4.0"), "https://www.kabum.com.br/produto/111107/placa-m-e-asrock-b450m-hdv-r4-0-amd-am4-micro-atx-ddr4-")
-	preco = req.getPreco(p)
-	print(preco)
-	assert type(preco) is float
+	l = rep_loja.add(l)
+	l = rep_loja.getById(l._id)
+	p = Produto(l, Variacao(Modelo(Marca(""), Arquitetura("", 0), 2020, "teste"), "AMD Ryzen 5 1600"), "https://www.kabum.com.br/produto/107545/processador-amd-ryzen-5-1600-cache-19mb-3-2ghz-3-6ghz-max-turbo-am4-yd1600bbafbox")
+	p = rep_produto.add(p)
+	p = rep_produto.getById(p._id)
+	preco = req.fetchPreco(p)
+	assert type(preco) is Produto
 
 def test_getProdutosPrecos():
 	if main.fb_app is None:
 		main.defineConfigs()
-	precos = req.getProdutos()
-	print(precos)
-	assert type(precos) is list
+	assert req.getProdutos() is None
