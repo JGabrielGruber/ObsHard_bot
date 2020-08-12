cf58684da4eef3c1877c37f7f23d4d941fe7cb20
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-08-12 09:44:40 -0300

    Implemented stock handling;

--- obsgpu_bot/price_request/req.py
+++ obsgpu_bot/price_request/req.py
@@ -29,7 +29,7 @@ async def fetchPreco(produto: Produto, key: str, loja: Loja, semaphore,
                      session, callback):
 	if produto and loja:
 		locale.setlocale(locale.LC_NUMERIC, "pt_BR.UTF-8")
-		price = {}
+		price = None
 		val = 0.0
 		status = 'ok'
 		try:
@@ -40,25 +40,30 @@ async def fetchPreco(produto: Produto, key: str, loja: Loja, semaphore,
 			price = soup.find(loja.tag,
 			                  attrs={loja.propriedade: loja.atributo})
 			if price != None:
-				if loja.nome == 'Kabum':
+				if loja.nome == 'Kabum' && soup.find('div',
+			                  attrs={'class': 'botao-comprar'}) != None:
 					val = float(price["content"])
-				elif loja.nome == 'Pichau':
+				elif loja.nome == 'Pichau' && soup.find('div',
+			                  attrs={'class': 'stock available'}) != None:
 					val = locale.atof(price.contents[1].string.split('R$')[1])
-				elif loja.nome == 'TerabyteShop':
+				elif loja.nome == 'TerabyteShop' && soup.find('div',
+			                  attrs={'id': 'indisponivel'}) == None:
 					val = locale.atof(price.contents[0])
+				else:
+					status = 'no'
 		except ValueError:
 			try:
 				val = locale.atof(price["content"])
 			except KeyError:
 				val = locale.atof(price.contents[0].string.split(" ", 1)[1])
 		except ConnectionError:
-			status = 'off'
+			status = 'of'
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 			logging.info(e)
 			logging.error(exc_type, fname, exc_tb.tb_lineno)
-			status = 'err'
+			status = 'er'
 		finally:
 			produto.status = status
 			if val > 0.0 and (
